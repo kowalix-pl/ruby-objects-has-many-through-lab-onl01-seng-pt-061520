af5e237eb2cd022a3ba3bf631e637d9c61917e83
kowalix007 L.K <kowalix007@gmail.com>
2020-07-13 09:42:42 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -16,7 +16,7 @@ class Patient
   end 
   
   def appointments 
-    Appointment.all.select |appointment| 
+    Appointment.all.select do |appointment| 
      appointment.patient == self
   end 
   
